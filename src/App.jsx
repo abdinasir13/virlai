053d221ae1e7b6b8d16f5e57bdf382bc2573ee91
abdinasir13 <61417745+abdinasir13@users.noreply.github.com>
2026-01-16 19:33:20 -0500

    Change export to App component
    Refactor export to use App component wrapping ViralMomentDetectorV2.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// ... all your component code from ViralMomentDetectorV2 artifact ...
+
+// At the very end, change this:
+// export default ViralMomentDetectorV2;
+
+// To this:
+export default function App() {
+  return <ViralMomentDetectorV2 />;
+}
